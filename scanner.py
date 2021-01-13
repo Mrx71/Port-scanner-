fe7fded0959962112799228c1421f619791d5226
Mrx71 <68593165+Mrx71@users.noreply.github.com>
2021-01-13 19:45:56 +0530

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,47 @@
+#scanner port
+
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+if len(sys.argv) == 2:
+    	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+	print("Invalid amount of arguments.")
+	print("syntex: python3 scanner.py <ip>")
+
+
+#Add a pretty banner
+
+print("-" * 50)
+print("scanning target " +target)
+print("Time started: "+str(datetime.now()))
+print("-" * 50)
+
+try:
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port)) #returns an error indicator
+		if result == 0:
+			print("Port {} is open".format(port))
+	s.close()
+
+except keyboardInterrupt:
+	print("\n Exiting program.")
+	sys.exit()
+
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
+
+
+              
+
